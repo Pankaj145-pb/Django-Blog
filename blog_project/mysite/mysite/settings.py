45682ee3c68d049516ad829fccb3cb72df62ab54
Pankaj145-pb <65770464+Pankaj145-pb@users.noreply.github.com>
2020-08-18 17:21:21 +0530

    Update settings.py

--- blog_project/mysite/mysite/settings.py
+++ blog_project/mysite/mysite/settings.py
@@ -19,8 +19,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l6!l+%t_mk)4ni=tn@pgnfg=4+f&8ps(c)lj*ttr)phlc^g%yv'
+# SECURITY WARNING: keep the secret key used in production secret!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
